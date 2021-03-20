46327cd616683585179b5305462d895c9f98b5d0
Rishi Kothari <itsrishikothari@gmail.com>
2021-03-20 11:35:41 -0400

    reconciliation & render

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import {h} from "snabbdom/build/package/h"
 import { VNode } from 'snabbdom/build/package/vnode'
+import { init } from 'snabbdom/build/package/init'
+import {propsModule} from 'snabbdom/build/package/modules/props'
 
 const createElement = (type: string, props: Record<string, unknown> = {}, ...children: VNode[] ) => {
     console.log(type, props, children);
@@ -8,8 +10,19 @@ const createElement = (type: string, props: Record<string, unknown> = {}, ...chi
 
 const Box = { createElement }
 
+const reconcile = init([propsModule])
+
+const render = (element: VNode, root: VNode | Element) => {
+    reconcile(root, element)
+}
+
+const BoxDOM = { render }
+
 const App = (
     <div>
         <p>Makin' makin' makin cool stuff with React!</p>
     </div>
 );
+
+
+BoxDOM.render(App as unknown as VNode, document.getElementById("root"))
