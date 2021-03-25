c1aad0024ef6559aa448295bfafbc6e2557ea88b
Rishi Kothari <itsrishikothari@gmail.com>
2021-03-25 18:13:17 -0400

    webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	devtool: 'eval-cheap-module-source-map',
-	entry: './src/index.ts',
+	entry: './src/index.tsx',
 	devServer: {
 		port: 3000,
 		contentBase: path.join(__dirname, 'dist'),
